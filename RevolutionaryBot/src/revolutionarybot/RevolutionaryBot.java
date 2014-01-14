1d3f9d3978154406b4ef9b60beeafcf84c4f0eb6
Forconi <manon.forconi@etu.univ-poitiers.fr>
2014-01-14 09:08:08 +0100

    Launch windows 1

--- RevolutionaryBot/src/revolutionarybot/RevolutionaryBot.java
+++ RevolutionaryBot/src/revolutionarybot/RevolutionaryBot.java
@@ -17,6 +17,7 @@ public class RevolutionaryBot {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        LaunchWindow w = new LaunchWindow();
     }
     
 }
